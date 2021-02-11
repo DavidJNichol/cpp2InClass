d6c85bd246de67c1ed2d2d69467f21778b8bc4d7
Alex Dimitrijevic <baiomurang@gmail.com>
2021-02-10 22:22:15 -0600

    adds make files with main and card.cpp

---
+++ week2/build/CMakeFiles/EuchreCPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/EuchreCPP.dir/src/main.cpp.o"
+  "EuchreCPP.pdb"
+  "EuchreCPP"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/EuchreCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
